5824af810b434a253e773d0b105b18f323aa9eaf
TimoxaV <vadim2493@gmail.com>
2020-09-14 17:30:42 +0300

    hw-2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Branch 1");
         System.out.println("New Branch 1");
+        System.out.println("Branch 2");
     }
 }
